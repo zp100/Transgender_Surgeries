c7b0abfc142275d5de4261ba7841493ac0f8bc43
Zach Preston <zachattack6089@gmail.com>
2025-02-05 22:23:57 -0800

    Finally fixed Vercel deployment I think...

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const { join } = require('path')
 const app = express()
 app.set('views', './api/views')
 app.set('view engine', 'pug')
-app.use('/static', express.static(join(__dirname, 'static')))
+app.use('/static', express.static(join(process.cwd(), 'api', 'static')))
 const PORT = 3001
 
 app.get('/', (req, res) => {
@@ -14,13 +14,15 @@ app.get('/', (req, res) => {
         res.redirect('/?page=TransWiki/wiki/index/index.md')
         return
     } else if (![ 'TransWiki', 'TransSurgeriesWiki' ].includes(req.query['page'].split('/')[0])) {
+        console.error(`Invalid page ${req.query['page']}`)
         res.sendStatus(404)
         return
     }
 
-    const filePath = join(__dirname, '..', req.query['page'])
+    const filePath = join(process.cwd(), req.query['page'])
     stat(filePath, (err) => {
         if (err != null) {
+            console.error(`Failed to find file with path ${filePath}`)
             res.sendStatus(404)
             return
         } else {
@@ -33,8 +35,12 @@ app.get('/', (req, res) => {
         
                 const markdownHtml = marked.parse(data, { gfm: true }).replaceAll(
                     'https://github.com/zp100/Transgender_Surgeries/blob/main/',
-                    `${req.protocol}://${req.hostname}:3000/?page=`
+                    (req.hostname === 'localhost'
+                        ? 'http://localhost:3000/?page='
+                        : `${req.protocol}://${req.hostname}/?page=`
+                    )
                 )
+
                 res.render('index', { markdownTitle: filePath.split('/').at(-1), markdownHtml })
             })
         }
