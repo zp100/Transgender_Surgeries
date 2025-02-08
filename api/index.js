7651f8dca6dd2626d176e548ab6534d58fd97395
Zach Preston <zachattack6089@gmail.com>
2025-02-08 12:55:09 -0800

    Moved wiki pages into sub-folder.

--- api/index.js
+++ api/index.js
@@ -9,23 +9,26 @@ app.set('views', './api/views')
 app.set('view engine', 'pug')
 app.use('/static', express.static(join(process.cwd(), 'api', 'static')))
 const PORT = 3001
+const WIKI_INDEX = 'TransWiki/wiki/index/index.md'
 
-// All requests go to the root URL, with a "page" query parameter added on.
-// E.g.: {hostUrl}/?page=TransWiki/wiki/index/index.md
-app.get('/', (req, res) => {
-    if (!('page' in req.query)) {
-        // If they're not on a page (such as when first arriving at the site), redirect to the index page.
-        res.redirect('/?page=TransWiki/wiki/index/index.md')
-        return
-    } else if (![ 'TransWiki', 'TransSurgeriesWiki' ].includes(req.query['page'].split('/')[0])) {
-        // If the "page" param specifies a file that doesn't exist in the wiki, raise an error.
+app.get('/', (_, res) => {
+    res.redirect(`/wiki?page=${WIKI_INDEX}`)
+    return
+})
+
+app.get('/wiki', (req, res) => {
+    if (!(
+        'page' in req.query &&
+        [ 'TransWiki', 'TransSurgeriesWiki' ].includes(req.query['page'].split('/')[0])
+    )) {
+        // If the "page" param is missing or specifies a file that doesn't exist in the wiki, raise an error.
         console.error(`Invalid page ${req.query['page']}`)
         res.sendStatus(404)
         return
     }
 
     // Check if the "page" param is a valid file.
-    const filePath = join(process.cwd(), req.query['page'])
+    const filePath = join(process.cwd(), 'wiki', req.query['page'])
     stat(filePath, (err) => {
         if (err != null) {
             // Raise an error and tell the server what the attempted file was.
@@ -47,13 +50,17 @@ app.get('/', (req, res) => {
                     // Replace all GitHub links to instead stay within whatever site is hosting this.
                     'https://github.com/zp100/Transgender_Surgeries/blob/main/',
                     (req.hostname === 'localhost'
-                        ? `http://localhost:${PORT}/?page=`
-                        : `${req.protocol}://${req.hostname}/?page=`
+                        ? `http://localhost:${PORT}/wiki?page=`
+                        : `${req.protocol}://${req.hostname}/wiki?page=`
                     )
                 )
 
                 // Insert the HTML into the template and render it.
-                res.render('index', { markdownTitle: filePath.split('/').at(-1), markdownHtml })
+                res.render('index', {
+                    markdownTitle: filePath.split('/').at(-1),
+                    markdownHtml,
+                    wikiIndex: WIKI_INDEX
+                })
             })
         }
     })
