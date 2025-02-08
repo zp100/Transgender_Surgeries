73b33531afcc02ed69aceb1ae5ead5bdab16fa79
Zach Preston <zachattack6089@gmail.com>
2025-02-08 13:18:15 -0800

    Switched from query param to route param.

--- api/index.js
+++ api/index.js
@@ -11,29 +11,26 @@ app.use('/static', express.static(join(process.cwd(), 'api', 'static')))
 const PORT = 3001
 const WIKI_INDEX = 'TransWiki/wiki/index/index.md'
 
+// Redirect the first site visit to the wiki index.
 app.get('/', (_, res) => {
-    res.redirect(`/wiki?page=${WIKI_INDEX}`)
+    res.redirect(`/wiki/${WIKI_INDEX}`)
     return
 })
 
-app.get('/wiki', (req, res) => {
-    if (!(
-        'page' in req.query &&
-        [ 'TransWiki', 'TransSurgeriesWiki' ].includes(req.query['page'].split('/')[0])
-    )) {
-        // If the "page" param is missing or specifies a file that doesn't exist in the wiki, raise an error.
-        console.error(`Invalid page ${req.query['page']}`)
-        res.sendStatus(404)
-        return
-    }
-
-    // Check if the "page" param is a valid file.
-    const filePath = join(process.cwd(), 'wiki', req.query['page'])
-    stat(filePath, (err) => {
+// Get wiki pages.
+app.get('/wiki*', (req, res) => {
+    // Check if the param is a valid file.
+    const filePath = join(process.cwd(), 'wiki', req.params['0'])
+    stat(filePath, (err, stats) => {
         if (err != null) {
             // Raise an error and tell the server what the attempted file was.
-            console.error(`Failed to find file with path ${filePath}`)
+            console.error(`Failed to get file with path ${filePath}`)
             res.sendStatus(404)
+            return
+        } else if (!stats.isFile()) {
+            // Raise an error and tell the server what the attempted file was.
+            console.error(`Not a regular file at path ${filePath}`)
+            res.sendStatus(400)
             return
         } else {
             // Read the file's markdown.
@@ -50,8 +47,8 @@ app.get('/wiki', (req, res) => {
                     // Replace all GitHub links to instead stay within whatever site is hosting this.
                     'https://github.com/zp100/Transgender_Surgeries/blob/main/',
                     (req.hostname === 'localhost'
-                        ? `http://localhost:${PORT}/wiki?page=`
-                        : `${req.protocol}://${req.hostname}/wiki?page=`
+                        ? `http://localhost:${PORT}/wiki/`
+                        : `${req.protocol}://${req.hostname}/wiki/`
                     )
                 )
 
@@ -66,10 +63,7 @@ app.get('/wiki', (req, res) => {
     })
 })
 
-// Loggin.
+// Logging.
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}.`)
 })
-
-// Export for Vercel. (This might not be needed? Doesn't hurt anything, though.)
-module.exports = app
