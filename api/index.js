46d3b201c0da65856f278f432813fd39a7ccc01d
Zach Preston <zachattack6089@gmail.com>
2025-02-10 00:09:05 -0800

    Refactored code.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { marked } = require('marked')
-const { readFile, stat } = require('node:fs')
 const { join } = require('path')
+const { fileRequestCallback } = require('./util')
 
 // App setup (configuring file access, port number, etc.).
 const app = express()
@@ -14,89 +14,27 @@ const WIKI_INDEX = 'r/TransWiki/wiki/index/content.md'
 // Redirect the first site visit to the wiki index.
 app.get('/', (_, res) => {
     res.redirect(`/wiki/${WIKI_INDEX}`)
-    return
 })
 
 // Get wiki pages.
 app.get('/wiki*', (req, res) => {
-    // Check if the param is a valid file.
-    const filePath = join(process.cwd(), 'wiki', req.params['0'])
-    stat(filePath, (err, stats) => {
-        if (err != null) {
-            // Raise an error and tell the server what the attempted file was.
-            console.error(`Failed to get file with path ${filePath}`)
-            res.sendStatus(404)
-            return
-        } else if (!stats.isFile()) {
-            // Raise an error and tell the server what the attempted file was.
-            console.error(`Not a regular file at path ${filePath}`)
-            res.sendStatus(400)
-            return
-        } else {
-            // Read the file's markdown.
-            readFile(filePath, 'utf8', (err, data) => {
-                if (err) {
-                    // Raise an error (this probably won't ever happen, but just in case).
-                    console.error(err)
-                    res.sendStatus(500)
-                    return
-                }
+    function markdownCallback(data) {
+        // Replace all GitHub links to instead stay within whatever site is hosting this.
+        const relativeData = data.replaceAll(
+            /\[(.*?)\]\((https:\/\/github\.com\/zp100\/Transgender_Surgeries\/blob\/main\/wiki\/)(.*?)\)/g,
+            '<span class="internal">[$1](/wiki/$3)</span>'
+        )
 
-                // Replace all GitHub links to instead stay within whatever site is hosting this.
-                const relativeData = data.replaceAll(
-                    /\[(.*?)\]\((https:\/\/github\.com\/zp100\/Transgender_Surgeries\/blob\/main\/wiki\/)(.*?)\)/g,
-                    '<span class="internal">[$1](/wiki/$3)</span>'
-                )
+        // Convert the markdown to HTML.
+        const markdownHtml = marked.parse(relativeData, { gfm: true })
+        return markdownHtml
+    }
 
-                // Convert the markdown to HTML.
-                const markdownHtml = marked.parse(relativeData, { gfm: true })
-
-                // Insert the HTML into the template and render it.
-                res.render('wiki', {
-                    title: filePath.split('/').at(-2),
-                    wikiIndex: WIKI_INDEX,
-                    markdownHtml
-                })
-            })
-        }
-    })
+    fileRequestCallback(req, res, WIKI_INDEX, 'wiki', 'md', markdownCallback)
 })
 
 // Get posts and comments.
-app.get('/posts*', (req, res) => {
-    // Check if the param is a valid file.
-    const filePath = join(process.cwd(), 'posts', req.params['0'])
-    stat(filePath, (err, stats) => {
-        if (err != null) {
-            // Raise an error and tell the server what the attempted file was.
-            console.error(`Failed to get file with path ${filePath}`)
-            res.sendStatus(404)
-            return
-        } else if (!stats.isFile()) {
-            // Raise an error and tell the server what the attempted file was.
-            console.error(`Not a regular file at path ${filePath}`)
-            res.sendStatus(400)
-            return
-        } else {
-            // Read the file's HTML.
-            readFile(filePath, 'utf8', (err, data) => {
-                if (err) {
-                    // Raise an error (this probably won't ever happen, but just in case).
-                    console.error(err)
-                    res.sendStatus(500)
-                    return
-                }
-
-                // Insert the HTML into the template and render it.
-                res.render('posts', {
-                    title: filePath.split('/').at(-2),
-                    wikiIndex: WIKI_INDEX,
-                    postHtml: data
-                })
-            })
-        }
-    })
-})
+app.get('/posts*', (req, res) => fileRequestCallback(req, res, WIKI_INDEX, 'posts'))
 
 // Logging.
 app.listen(PORT, () => {
---
+++ api/util.js
@@ -0,0 +1,44 @@
+const { readFile, stat } = require('node:fs')
+const { join } = require('path')
+
+// Fetch and process the page specified by a particular request.
+// Generalized to apply to all pages in all sections (wiki, posts, etc.).
+export function fileRequestCallback(req, res, wikiIndex, section, extension = 'html', dataCallback = ((data) => data)) {
+    // Check if the param is a valid file.
+    const fileName = req.params['0']
+    const filePath = join(process.cwd(), section, fileName)
+    stat(filePath, (err, stats) => {
+        if (err != null) {
+            // Raise an error and tell the server what the attempted file was.
+            console.error(`Failed to get file with path ${filePath}`)
+            res.sendStatus(404)
+        } else if (stats.isDirectory()) {
+            // Go to the content file for this directory (if it exists).
+            res.redirect(`/posts/${fileName}/content.${extension}`)
+        } else if (!stats.isFile()) {
+            // Raise an error and tell the server what the attempted file was.
+            console.error(`Not a regular file at path ${filePath}`)
+            res.sendStatus(400)
+        } else {
+            // Read the file's content.
+            readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    // Raise an error (this probably won't ever happen, but just in case).
+                    console.error(err)
+                    res.sendStatus(500)
+                    return
+                }
+
+                // Run callback for this file.
+                const contentHtml = dataCallback(data)
+
+                // Insert the HTML into the template and render it.
+                res.render('posts', {
+                    title: filePath.split('/').at(-2),
+                    wikiIndex,
+                    contentHtml
+                })
+            })
+        }
+    })
+}
